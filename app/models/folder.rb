e2747ef5670c6cf5440830891ba953d74b79e4ca
Ashley Schauer <aelschauer@gmail.com>
2017-03-28 12:46:50 -0600

    add relationships and ancestors method

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,2 +1,14 @@
 class Folder < ApplicationRecord
+  belongs_to :user
+  has_many :children, :class_name => 'Folder', :foreign_key => 'parent_id'
+  belongs_to :parent, :class_name => 'Folder', optional: true
+  has_many :uploads
+
+  def ancestors
+    ancestors = [self]
+    unless parent_id.zero?
+      ancestors.concat(Folder.find(parent_id).ancestors)
+    end
+    ancestors
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :folders
+  has_many :uploads, through: :folders
 end
