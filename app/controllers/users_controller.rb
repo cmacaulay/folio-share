f0b53171df904602529c9d19c4507b5b5ccb59bf
Alex Fosco <alex.hf88@gmail.com>
2017-03-28 17:40:06 -0600

    adds guest can create an account

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :cellphone, :password)
+  end
+end
---
+++ db/migrate/20170328224926_drop_username_from_users.rb
@@ -0,0 +1,5 @@
+class DropUsernameFromUsers < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :username
+  end
+end
