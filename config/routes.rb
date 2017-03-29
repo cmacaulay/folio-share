84a36b02cac2ee7502bc2946544bb65d4b1f78ed
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-29 00:23:19 -0600

    add navbar styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get "/", to: "welcome#show"
+
   resources :users, only: [:new, :create, :edit, :update]
 
-  get '/home', to: 'users#show'
+  get "/home", to: "users#show"
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
 end
