d8d91f5bbbb212887ff99bec7c69501ed50607bd
Ashley Schauer <aelschauer@gmail.com>
2017-03-29 00:59:43 -0600

    fix comma hound errors

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     first_name 'Alex'
     last_name 'Smith'
     password 'password'
-    sequence(:cellphone) { |n| "303987#{n.to_s.rjust(4,'0')}" }
+    sequence(:cellphone) { |n| "303987#{n.to_s.rjust(4, '0')}" }
     sequence(:email) { |n| "alexsmith#{n}@example.com" }
     status 0
   end
