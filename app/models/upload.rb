2e3c6f338ad5fe6a8c42b56f73e165ff9c0758dc
Ashley Schauer <aelschauer@gmail.com>
2017-03-29 16:28:34 -0600

    update phone number in user factory and method to get user from upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,12 +3,9 @@ class Upload < ApplicationRecord
   validates_attachment_content_type :file, content_type: all
 
   belongs_to :folder
+  delegate :user, to: :folder
 
   alias_attribute :name, :file_file_name
   alias_attribute :content_type, :file_content_type
   alias_attribute :size, :file_file_size
-
-  def user
-    folder.user
-  end
 end
