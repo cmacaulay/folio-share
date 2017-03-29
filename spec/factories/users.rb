b9eb0e1c953ab59a453969c1b7130e2bd3309c3b
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-28 19:28:17 -0600

    all tests passing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:username) { |n| "doglover#{n}" }
+    sequence(:first_name) { |n| "Michael#{n}" }
+    last_name "Jackson"
     password "password"
     sequence(:cellphone) { |n| "303-987-#{n.to_s.rjust(4,"0")}" }
     sequence(:email) { |n| "doglover#{n}@example.com" }
