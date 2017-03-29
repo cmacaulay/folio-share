459e3aa117e4fe8bcce930393d63252f182f6b58
Ashley Schauer <aelschauer@gmail.com>
2017-03-29 17:22:34 -0600

    fix hound errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
   def user_params
     params
-    .require(:user)
-    .permit(:username, :first_name, :last_name, :email, :cellphone, :password)
+      .require(:user)
+      .permit(:username, :first_name, :last_name, :email, :cellphone, :password)
   end
 end
