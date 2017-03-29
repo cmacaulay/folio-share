a7932a388dc130539846719b16a1a5a2ca8c1151
Ashley Schauer <aelschauer@gmail.com>
2017-03-29 14:16:27 -0600

    add basic code to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  first = Faker::Name.first_name
+  last = Faker::Name.last_name
+  User.create(
+    first_name: first,
+    last_name: last,
+    cellphone: Faker::PhoneNumber.phone_number,
+    email: Faker::Internet.safe_email("#{first}.#{last}"),
+    password: "password"
+  )
+end
+
+binding.pry
+
+User.all.each do |user|
+  rand(0..5).times do
+    user.folders.create(
+      name: "Folder #{Faker::Lorem.word}",
+      parent_id: user.folders.sample
+    )
+  end
+  user.folders.each do |folder|
+    rand(0..5).times do
+      folder.uploads.create(
+        name: "File #{Faker::Lorem.word}",
+        content_type: Faker::File.mime_type,
+        size: rand(1..10000)
+      )
+    end
+  end
+end
