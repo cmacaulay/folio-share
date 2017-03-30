36e0f03e809df4c5d95e1bec6f357a7f588ad3ed
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-30 14:47:02 -0600

    add config for aws region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      s3_region => ENV.fetch('AWS_REGION'),
     }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
