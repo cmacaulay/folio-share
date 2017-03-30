2ae90e950d9740a70d9bcce027de67e7d9e1f54d
Ashley Schauer <aelschauer@gmail.com>
2017-03-29 23:13:47 -0600

    remove duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   get "/", to: "welcome#show"
-
-  resources :users, only: [:new, :create, :edit, :update]
-
   get "/home", to: "users#show"
-
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
   resources :users, only: [:new, :create, :edit, :update]
-
   resources :folders, path: :f, only: [:show, :new, :create]
   resources :uploads, path: :u, only: [:show, :new, :create]
 end
