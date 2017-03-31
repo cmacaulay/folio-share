3bccd206beba3aa84170eab27a5fd8b0f9056aa8
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-03-30 23:23:41 -0600

    removes sensitive data

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -14,7 +14,7 @@ class TwilioService
     @client = Twilio::REST::Client.new account_sid, auth_token
     message = @client.account.messages.create(
       from: "+17207702071",
-      to: "+17209826950",
+      to: @cellphone
       body: "Your reset code is #{@user.reset_token}"
     )
   end
