e6c538f6c0e8b5749c4a3b500ddd0b17af35b3b1
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-04-01 17:37:33 -0600

    fix comment bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.upload_id = params[:upload_id]
+    @comment.user  = current_user
     @comment.save
     redirect_to upload_path(@comment.upload)
   end
