b40bbe51c8e64e79120f2445caab951d53bc3c72
Alex Fosco <alex.hf88@gmail.com>
2017-04-02 14:19:05 -0600

    adds styling to comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   get "/", to: "welcome#show"
-  get "/home", to: "users#index"
+  get "/home", to: "users#show"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-
-#admin
-  namespace :admin do
-    get '/dashboard', to: 'dashboard#dashboard'
-  end
 
 #password reset
   get '/password', to: 'passwords#new'
@@ -16,8 +11,7 @@ Rails.application.routes.draw do
   get '/password/update', to: 'passwords#edit', as: "reset_password"
   put '/password/update', to: 'passwords#update', as: "update_password"
 
-
-  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :users, only: [:new, :create, :edit, :update]
   resources :folders, path: :f, only: [:show]
   resources :folders, path: "f/:id", only: [:new, :create]
   # resources :folders, path: :f, only: [:show, :new, :create]
