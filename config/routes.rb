a42d899eaa0ad38a76b0d864a9bc57c27ee20111
Ashley Schauer <aelschauer@gmail.com>
2017-04-01 19:49:28 -0600

    adds zip generator for download

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  namespace :uploads do
+    get 'download/index'
+  end
+
+  namespace :folders do
+    get 'download/index'
+  end
+
+  namespace :uploads do
+    get 'download/create'
+  end
+
+  namespace :folders do
+    get 'download/create'
+  end
+
   get "/", to: "welcome#show"
   get "/home", to: "users#show"
   get "/login", to: "sessions#new"
@@ -10,10 +26,11 @@ Rails.application.routes.draw do
   post '/password', to: 'passwords#create'
   get '/password/update', to: 'passwords#edit', as: "reset_password"
   put '/password/update', to: 'passwords#update', as: "update_password"
-  
+
   resources :users, only: [:new, :create, :edit, :update]
   resources :folders, path: :f, only: [:show]
   resources :folders, path: "f/:id", only: [:new, :create]
-  # resources :folders, path: :f, only: [:show, :new, :create]
+  get "/f/:id/download", to: "folders/download#index", as: "folder_download"
   resources :uploads, path: :u, only: [:show, :new, :create]
+  get "/u/:id/download", to: "uploads/download#index", as: "upload_download"
 end
