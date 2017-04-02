d687b0eb4d68725860a9e6aae6a826bbeec22433
Alex Fosco <alex.hf88@gmail.com>
2017-04-02 16:53:13 -0600

    adds styling for comments on upload show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
+    @comment = current_user.comments.new(comment_params)
     @comment.upload_id = params[:upload_id]
-    @comment.user  = current_user
+    # @comment.user = current_user
     @comment.save
     redirect_to upload_path(@comment.upload)
   end
