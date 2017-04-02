ceec3e11849e864cdc015ecfcf6c501581125689
Ashley Schauer <aelschauer@gmail.com>
2017-04-01 19:48:36 -0600

    add download controllers for folder and upload

---
+++ app/helpers/folders/download_helper.rb
@@ -0,0 +1,2 @@
+module Folders::DownloadHelper
+end
---
+++ spec/controllers/folders/download_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Folders::DownloadController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      root = create(:folder)
+      get :index, params: { id: root.id }
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
