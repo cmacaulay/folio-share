b084c1db57f912602bd442f09b50185ecb30e622
Ashley Schauer <aelschauer@gmail.com>
2017-04-03 11:54:57 -0600

    fixes bug when user downloads a folder without any uploads

--- app/controllers/folders/download_controller.rb
+++ app/controllers/folders/download_controller.rb
@@ -2,9 +2,15 @@ class Folders::DownloadController < ApplicationController
   def index
     folder = Folder.find(params[:id])
     zip = ZipGenerator.new(folder)
-    zip.download
+    result = zip.download
 
-    send_data(zip.data, type: "application/zip", filename: zip.file_name)
+    if result == :success
+      send_data(zip.data, type: "application/zip", filename: zip.file_name)
+    elsif result == :failure
+      flash[:danger] = "You cannot download an empty folder."
+      redirect_to request.env["HTTP_REFERER"]
+    end
+
     zip.close
   end
 end
