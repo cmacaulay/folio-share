efec5ad7c87f90622f04ee0249802d300fd429ef
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-04-02 20:37:27 -0600

    adds link to admin dashboard page if admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,5 +65,7 @@ class User < ApplicationRecord
     self.deactivated
   end
 
-
+  def full_name
+  first_name.capitalize + " " + last_name.capitalize
+  end
 end
---
+++ spec/features/admin/admin_clicks_admin_navbar_link_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature "admin visit user index(show) page" do
+  describe "clicks admin link" do
+    it "redirects to admin dashboard" do
+      admin = create(:user)
+      admin.roles.create(name: "admin")
+
+      controller = ApplicationController
+      allow_any_instance_of(controller).to receive(:current_user).and_return(admin)
+
+      visit home_path
+
+      click_on "Admin"
+      expect(current_path).to eq(admin_dashboard_path)
+      expect(page).to have_content("Admin Dashboard")
+    end
+  end
+end
