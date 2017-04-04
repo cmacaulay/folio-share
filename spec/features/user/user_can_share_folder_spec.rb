143246821bf599d899e2d580819e8e1a94a569c4
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-04-03 22:28:20 -0600

    final test debug

--- spec/features/user/user_can_share_folder_spec.rb
+++ spec/features/user/user_can_share_folder_spec.rb
@@ -13,19 +13,27 @@ describe "a user can shares a folder with another user" do
             parent: user1.root_folder,
             name: "Code"
                     )
+
       visit "/"
       click_on "Login"
+
       fill_in "session[email]", with: user1.email
       fill_in "session[password]", with: user1.password
+
       click_button "Start Sharing"
+
       expect(current_path).to eq("/Folio")
       expect(page).to have_content(folder.name)
+
       click_on "Share Folder"
+
       fill_in "collaboration[user]", with: user2.username
       click_on "Collaborate"
+
       expect(page).to have_content("You shared Code with #{user2.username}!")
       expect(current_path).to eq "/f/#{folder.id}"
     end
+
     it "the collaborator sees the folder when they visit their homepage" do
       user1 = create(:user, id: 1)
       user2 = create(:user, id: 2)
@@ -61,6 +69,7 @@ describe "a user can shares a folder with another user" do
         expect(page).to have_content(user1.username)
       end
     end
+    
     it "won't successfully share if the username" do
       user1 = create(:user, id: 1)
       user1.roles.create(name: "registered user")
